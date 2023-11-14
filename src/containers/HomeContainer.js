6ebe9fdf768e2d732705f3ba1bb5733a47206d1d
vicocongui <vicufranco2001@gmail.com>
2023-11-14 10:32:40 -0300

    error listo

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,11 +1,10 @@
 import { useCartContext } from "@/contexts/Cartcontext";
-import CardsContainer from "@/containers/CardsContainer"
+import CardsContainer from "@/containers/CardsContainer";
+import { useState, useEffect } from "react";
 
 const HomeContainer = () => {
   const { products, lodingProducts } = useCartContext();
   const [recursos, setRecursos] = useState([]);
-  const [heladoPote, setHeladoPote] = useState([]);
-  const [heladoFranui, setHeladoFranui] = useState([]);
   //const productosFiltradosPalito = products.filter(producto=>producto.type === producto.type.includes("palito") )
 
   useEffect(() => {
@@ -15,17 +14,6 @@ const HomeContainer = () => {
       productosMasVendidos = [...products.franui.options, ...products.palito.options, ...products.pote.options]
       console.log(productosMasVendidos)
     }
-
-    /* const palitoHelado = products.filter((el) => el.type.includes("palito"));
-     console.log(palitoHelado, "palito");
-     const poteHelado = products.filter((el) => el.type.includes("pote"));
-     console.log(poteHelado, "pote");
-     const franuiHelado = products.filter((el) => el.type.includes("franui"));
-     console.log(franuiHelado, "franui");
- 
-     setHeladoPalito(palitoHelado);
-     setHeladoPote(poteHelado);
-     setHeladoFranui(franuiHelado);*/
     setRecursos(productosMasVendidos)
   }, [products]);
 
