639b63ce11c76b74a6432e21c5f673543ea3d39a
jimezapolski <jimezapolski@gmail.com>
2023-11-14 11:14:56 -0300

    public img

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,12 +1,11 @@
+import { useState, useEffect } from "react";
 import { useCartContext } from "@/contexts/Cartcontext";
 import CardsContainer from "@/containers/CardsContainer"
 
 const HomeContainer = () => {
   const { products, lodingProducts } = useCartContext();
   const [recursos, setRecursos] = useState([]);
-  const [heladoPote, setHeladoPote] = useState([]);
-  const [heladoFranui, setHeladoFranui] = useState([]);
-  //const productosFiltradosPalito = products.filter(producto=>producto.type === producto.type.includes("palito") )
+  
 
   useEffect(() => {
     console.log(products)
@@ -16,16 +15,7 @@ const HomeContainer = () => {
       console.log(productosMasVendidos)
     }
 
-    /* const palitoHelado = products.filter((el) => el.type.includes("palito"));
-     console.log(palitoHelado, "palito");
-     const poteHelado = products.filter((el) => el.type.includes("pote"));
-     console.log(poteHelado, "pote");
-     const franuiHelado = products.filter((el) => el.type.includes("franui"));
-     console.log(franuiHelado, "franui");
- 
-     setHeladoPalito(palitoHelado);
-     setHeladoPote(poteHelado);
-     setHeladoFranui(franuiHelado);*/
+    
     setRecursos(productosMasVendidos)
   }, [products]);
 
