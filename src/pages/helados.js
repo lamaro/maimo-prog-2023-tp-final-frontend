630f424b9d26c0a14d8421a03375b356d4d86916
Martina <mfranco0305@gmail.com>
2023-11-16 16:52:07 -0300

    category

--- src/pages/helados.js
+++ src/pages/helados.js
@@ -1,7 +1,7 @@
 import CardsContainer from '@/containers/CardsContainer';
 import React, { useEffect, useState } from 'react'
 import { useCartContext } from '@/contexts/Cartcontext';
-import { Button} from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 import LayoutContainer from '@/containers/LayoutContainer';
 
 const helados = () => {
@@ -34,29 +34,29 @@ const helados = () => {
         }
     }
 
-
     return (
+        
         <div className="bg-amber-50 min-h-screen">
-        <LayoutContainer> 
-            {/* Header */}
-            <header className="text-center p-4">
-                {/* Navigation here */}
-            </header>
-            {/* Main Content */}
-            <main className="py-10">
-                {/* Title */}
-                <h1 className="text-6xl font-bold text-center mb-10">Productos Gelatella</h1>
-                {/*FILTROS*/}
-                <div className='flex mb-8 justify-center'>
-                        <Button value={"franui"} variant="light" onClick={handleClick}>Gelatella x Franui</Button>
+            <LayoutContainer>
+                {/* Header */}
+                <header className="text-center p-4">
+                    {/* Navigation here */}
+                </header>
+                {/* Main Content */}
+                <main className="py-10">
+                    {/* Title */}
+                    <h1 className="text-6xl font-bold text-center mb-10">Productos Gelatella</h1>
+                    {/*FILTROS*/}
+                    <div className='flex my-8 justify-center'>
+                        <Button className="hover:bg-violet-600" value={"franui"} variant="light" onClick={handleClick}>Gelatella x Franui</Button>
                         <Button value={"palitos"} variant="light" onClick={handleClick}>Gelatella x Palitos</Button>
                         <Button value={"pote"} variant="light" onClick={handleClick}>Gelatella x Pote</Button>
-                </div>
-                {/* Products Section */}
-                <CardsContainer recursos={recursos} className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4">
-                </CardsContainer>
-            </main>
-        </LayoutContainer>
+                    </div>
+                    {/* Products Section */}
+                    <CardsContainer recursos={recursos} className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4">
+                    </CardsContainer>
+                </main>
+            </LayoutContainer>
         </div>
     );
 
