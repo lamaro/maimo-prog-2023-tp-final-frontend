ce97c4e32f2dddd4d572813375a6457b9b641d13
vicocongui <vicufranco2001@gmail.com>
2023-11-15 21:38:32 -0300

    agrego navbar y footer al single

--- src/componentes/ShowSingleProduct.js
+++ src/componentes/ShowSingleProduct.js
@@ -1,5 +1,6 @@
 import { CardHeader, Card, Image, CardBody, RadioGroup, Radio } from '@nextui-org/react'
 import React from 'react'
+import LayoutContainer from '@/containers/LayoutContainer'
 //allInfo es todo mi json
 export const ShowSingleProduct = ({ producto, allInfo }) => {
     console.log(producto)
@@ -7,37 +8,40 @@ export const ShowSingleProduct = ({ producto, allInfo }) => {
 
 
     return (
-        <div className='h-screen bg-black flex flex-col justify-center items-center'>
+        <LayoutContainer>
+            <div className='h-screen bg-black flex flex-col justify-center items-center'>
 
-            <Card className='bg-red-200 '>
-                <CardHeader>{producto.name}</CardHeader>
-                <div className='grid grid-cols-2'>
-                    <Image src='https://picsum.photos/700/400'></Image>
-                    <CardBody>
-                        Titulo
-                        Parrafo
-                        gustos
-                        <div>
-                            <RadioGroup
-                                label="Select your favorite city"
-                                value={selected}
-                                onValueChange={setSelected}
-                            >
-                                {/* {si tengo toda la info, entonces hace un map para todos los gustos} */}
-                                {allInfo && allInfo.gustos.options.map(gusto => {
-                                    return (
-                                        <Radio value={gusto.name} key={gusto.sku} >{gusto.name}</Radio>
-                                    )
-                                })}
+                <Card className='bg-red-200 '>
+                    <CardHeader>{producto.name}</CardHeader>
+                    <div className='grid grid-cols-2'>
+                        <Image src='https://picsum.photos/700/400'></Image>
+                        <CardBody>
+                            Titulo
+                            Parrafo
+                            gustos
+                            <div>
+                                <RadioGroup
+                                    label="Select your favorite city"
+                                    value={selected}
+                                    onValueChange={setSelected}
+                                >
+                                    {/* {si tengo toda la info, entonces hace un map para todos los gustos} */}
+                                    {allInfo && allInfo.gustos.options.map(gusto => {
+                                        return (
+                                            <Radio value={gusto.name} key={gusto.sku} >{gusto.name}</Radio>
+                                        )
+                                    })}
 
-                            </RadioGroup>
-                            Gusto Seleccionado: {selected}
-                        </div>
-                        Boton
-                    </CardBody>
-                </div>
+                                </RadioGroup>
+                                Gusto Seleccionado: {selected}
+                            </div>
+                            Boton
+                        </CardBody>
+                    </div>
 
-            </Card>
-        </div>
+                </Card>
+
+            </div>
+        </LayoutContainer >
     )
 }
