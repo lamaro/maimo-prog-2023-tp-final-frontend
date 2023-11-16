d585f69b5de6d57e35442004317a94cdb3dc5765
vicocongui <vicufranco2001@gmail.com>
2023-11-15 21:04:37 -0300

    arreglnado ando

--- src/containers/SingleProductContainer.js
+++ src/containers/SingleProductContainer.js
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
-
 import { useCartContext } from '@/contexts/Cartcontext';
 import { ShowSingleProduct } from '@/componentes/ShowSingleProduct';
 import { Spinner } from '@nextui-org/react';
 
 const SingleProductContainer = ({ id }) => {
-  const { producto, getProduct, loadingProducts,products } = useCartContext()
+  const { producto, getProduct, loadingProducts, products } = useCartContext()
   console.log("id: ",id)
   useEffect(() => {
     if (id) {
@@ -14,8 +13,10 @@ const SingleProductContainer = ({ id }) => {
   }, [id, getProduct]);
 
   //boton agregar al carrito allInfo para que traiga los gusto, producto para que tariga los productos
+  //allInfo = gustos
+  //Traigo los gustos porque no tienen sku
   return (
-    <>
+    <>                                                           
       {!loadingProducts ? <ShowSingleProduct producto={producto} allInfo={products} /> : <Spinner size="lg" /> }
      
       
