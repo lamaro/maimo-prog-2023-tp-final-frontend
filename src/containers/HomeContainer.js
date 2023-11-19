68457744ba290f34b21dda9dac0a3b4a018cfb60
jimezapolski <jimezapolski@gmail.com>
2023-11-19 18:49:14 -0300

    move useEffect y useState to cartContext

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,24 +1,11 @@
 import { useCartContext } from "@/contexts/Cartcontext";
 import CardsContainer from "@/containers/CardsContainer";
-import { useState, useEffect } from "react";
 import Hero from "@/componentes/Hero";
 import Conocenos from "@/componentes/Conocenos";
 import Form from "@/componentes/Form";
 
 const HomeContainer = () => {
-  const { products, lodingProducts } = useCartContext();
-  const [recursos, setRecursos] = useState([]);
-
-  useEffect(() => {
-    console.log(products)
-    let productosMasVendidos = []
-    if (products) {
-      productosMasVendidos = [...products.franui.options, ...products.palito.options, ...products.pote.options]
-      console.log(productosMasVendidos)
-    }
-    setRecursos(productosMasVendidos)
-  }, [products]);
-
+  const  { filterRecursos} = useCartContext();
   return (
     <div>
 
@@ -28,7 +15,7 @@ const HomeContainer = () => {
       <Conocenos />
       <section>
       <h1 className=' mb-6  font-medium text-center  md:text-3xl xl:text-6xl text-black'>Los más pedidos</h1>
-        <CardsContainer recursos={recursos}></CardsContainer>
+        <CardsContainer recursos={filterRecursos("todos")}></CardsContainer>
       </section>
       <section>
       <Form />
