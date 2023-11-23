06042ffebeffddec972d5a4302b91b80f7b9203f
jimezapolski <jimezapolski@gmail.com>
2023-11-23 01:46:26 -0300

    contact page

--- src/componentes/ProductCard.js
+++ src/componentes/ProductCard.js
@@ -1,9 +1,22 @@
+import Product from '@/pages/product/[slug]';
 import { Accordion, AccordionItem, Button, Card, CardBody, CardHeader, Image} from '@nextui-org/react';
 import React from 'react';
+import Link from "next/link";
 
-const ProductCard = ({ product }) => {
-
-
+const ProductCard = ({ product  }) => {
+    // console.log(slugName, "estoy aca")
+    let tipo;
+    if (product.name.includes("Pote")) {
+        tipo = "pote";
+    } else if (product.name.includes("Franuis")) {
+        tipo = "franui";
+    } else if (product.name.includes("Palito")) {
+        tipo = "palito";
+    } else {
+        // Si no coincide con ninguna de las opciones anteriores
+        tipo = "Otro";
+    }
+const slugName= tipo
     return (
         <Card className="py-4">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
@@ -23,7 +36,12 @@ const ProductCard = ({ product }) => {
                     <p className="text-tiny uppercase font-bold">{product?.units}</p>
                     <p className="text-tiny uppercase font-bold">{product?.maxFlavors}</p>
                 </div>
-                <Button color="green" variant="ghost" className= "text-tiny uppercase font-bold">Ver más</Button>
+                <Link
+  href={`/backoffice/products/[id]?id=${slugName}`}
+  className="mr-8 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-black rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+>
+  Ver más
+</Link>
                 
             </CardBody>
         </Card>
